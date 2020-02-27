bd6c642f4b4876da47b2ddb3bb7c84b6d946327c
cquil11 <60715037+cquil11@users.noreply.github.com>
2020-02-27 14:50:17 -0600

    Swing experimentation

--- src/autonstudio/java/GUI.java
+++ src/autonstudio/java/GUI.java
@@ -1,9 +1,35 @@
 package autonstudio.java;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GUI {
+
+    private ImageIcon imageA;
+    private JLabel labelA;
+
     public static void main ( String [] args ) {
-        System.out.print("hello world");
-        System.out.print("hello world");
+            JFrame frame = new JFrame("My First GUI");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(800,800);
+            JButton button = new JButton("Press");
+            frame.getContentPane().add(button); // Adds Button to content pane of frame
+            frame.setVisible(true);
+        }
     }
 
+/*class Image extends JFrame {
+    private ImageIcon imageA;
+    private JLabel labelA;
+    private ImageIcon imageB;
+    private JLabel labelB;
+
+    public Image (){
+        setLayout(new FlowLayout());
+
+    }
 }
+*/
+
+
+
