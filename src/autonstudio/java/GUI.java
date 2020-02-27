90185a7801ffba5c3047164f0e65bd65b9348aee
cquil11 <60715037+cquil11@users.noreply.github.com>
2020-02-27 09:48:54 -0600

    Added main GUI class

--- src/autonstudio/java/GUI.java
+++ src/autonstudio/java/GUI.java
@@ -3,6 +3,7 @@ package autonstudio.java;
 public class GUI {
     public static void main ( String [] args ) {
         System.out.print("hello world");
+        System.out.print("hello world");
     }
 
 }
