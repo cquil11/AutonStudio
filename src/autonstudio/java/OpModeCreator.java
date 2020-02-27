4b4c380d881a532d2900d33ed6e1bbc0f3176c12
Matthew Kozlowski <mkozlowski225@gmail.com>
2020-02-27 13:43:59 -0600

    Added OpModeCreator

--- src/autonstudio/java/OpModeCreator.java
+++ src/autonstudio/java/OpModeCreator.java
@@ -1,4 +1,20 @@
 package autonstudio.java;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class OpModeCreator {
+    public static void main(String[] args) throws IOException {
+        File file = new File("file.txt");
+        file.createNewFile();
+
+        FileWriter fw = new FileWriter(file);
+        fw.write("package org.firstinspires.ftc.teamcode;\n");
+        fw.write("import com.qualcomm.robotcore.eventloop.opmode.Autonomous;\n");
+        fw.write("@Autonomous\npublic class AutonClassName extends PositionBasedAuton {\n");
+        fw.write("}");
+        fw.close();
+    }
+
 }
